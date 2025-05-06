d9c1b077603a83281459d5dcd219b243fb0f400a
Alexander <alexandermbugua4@gmail.com>
2025-05-06 14:28:35 +0300

    forth commit

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-import jsonfile from 'jsonfile';
-import moment from 'moment';
-import simpleGit from 'simple-git';
-import random from 'random';
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
 
-const FILE_PATH = './data.json';
-const git = simpleGit();
+const path = "./data.json";
 
-const makeCommits = async (n) => {
-  if (n === 0) {
-    try {
-      await git.push();
-      console.log('Pushed to remote repository');
-      return;
-    } catch (error) {
-      console.error('Error pushing to remote:', error);
-      return;
-    }
-  }
-
-  const x = random.int(0, 54); // weeks
-  const y = random.int(0, 6);  // days
+const markCommit = (x, y) => {
   const date = moment()
-    .subtract(1, 'y')
-    .add(1, 'd')
-    .add(x, 'w')
-    .add(y, 'd')
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
     .format();
 
   const data = {
-    date: date
+    date: date,
   };
 
-  try {
-    await jsonfile.writeFile(FILE_PATH, data);
-    await git.add(FILE_PATH);
-    await git.commit(date, FILE_PATH, { '--date': date });
-    console.log(`Commit ${n} created for ${date}`);
-    
-    // Recursively make next commit
-    await makeCommits(--n);
-  } catch (error) {
-    console.error('Error creating commit:', error);
-  }
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  });
 };
 
-// Start making commits
-makeCommits(150).then(() => {
-  console.log('All commits created!');
-});
+const makeCommits = (n) => {
+  if(n===0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+  const data = {
+    date: date,
+  };
+  console.log(date);
+  jsonfile.writeFile(path, data, () => {
+    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
+  });
+};
+
+makeCommits(150);
